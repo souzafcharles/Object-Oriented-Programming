939d0e4ee8bbaf33ee24b64f2260ed2c5bf810cf
Charles Fernandes de Souza <souzach@outlook.com>
2021-11-02 09:30:20 -0300

    Class 04 Examples

--- Class04/src/souza/charles/Dublador.java
+++ Class04/src/souza/charles/Dublador.java
@@ -1,5 +1,10 @@
 package souza.charles;
-
+/*
+Subject title: Object-Oriented Programming
+Lecturer: Prof. Dr. Lucas Bueno Ruas de Oliveira
+Example adapted by: Charles Fernandes de Souza
+Date: August 30, 2021
+*/
 public class Dublador {
     String nome;
     int registroProfissional;
