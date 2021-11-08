af738b033d5c57c0067e4c1e31653f85b9a73e12
Charles Fernandes de Souza <souzach@outlook.com>
2021-11-08 16:55:45 -0300

    Example Class 10

--- Class10/src/main/java/souza/charles/view/WindowReverter.java
+++ Class10/src/main/java/souza/charles/view/WindowReverter.java
@@ -1,5 +1,10 @@
 package souza.charles.view;
-
+/*
+Subject title: Object-Oriented Programming
+Lecturer: Prof. Dr. Lucas Bueno Ruas de Oliveira
+Example adapted by: Charles Fernandes de Souza
+Date: October 25, 2021
+*/
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
