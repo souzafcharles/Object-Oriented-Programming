68b05d1abaa277b34618590eaade06c2c0831db5
Charles Fernandes de Souza <souzach@outlook.com>
2021-11-08 17:02:28 -0300

    Practical Exercise: Abstractions and Collections

--- Class09/src/souza/charles/Revendedor.java
+++ Class09/src/souza/charles/Revendedor.java
@@ -1,5 +1,10 @@
 package souza.charles;
-
+/*
+Subject title: Object-Oriented Programming
+Lecturer: Prof. Dr. Lucas Bueno Ruas de Oliveira
+Example adapted by: Charles Fernandes de Souza
+Date: October 04, 2021
+*/
 public class Revendedor extends Funcionario{
 
     public Revendedor(String cpf, String nome, Integer idade, Boolean sexo, Double valorVendido, Consultor responsavel) {
